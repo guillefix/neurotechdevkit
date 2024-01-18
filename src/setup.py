23356cbd1d76f564ce889313ebc6096d4721deaf
Guillermo Valle <guillefix@gmail.com>
2024-01-18 07:28:14 +0000

    fixed setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,8 @@ setup(
     name='neurotechdevkit',
     version='0.6.1',
     packages=['neurotechdevkit'],
-    install_requires=[
-    "psutil==5.9.6",
-    "git+https://github.com/trustimaging/stride@2520c59",
-    ],
+    #install_requires=[
+    #"psutil==5.9.6",
+    #"git+https://github.com/trustimaging/stride@2520c59",
+    #],
 )
